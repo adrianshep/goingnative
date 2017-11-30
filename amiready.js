30dc6249d199b93682a0d10bfd16809ba95347eb
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-11-30 09:32:07 -0500

    1st lesson in goingnative

---
+++ amiready.js
@@ -0,0 +1,49 @@
+
+
+
+/*
+
+AM I READY?
+ Exercise 1 of 8
+
+──────────────────────────────────────────────────────────────────────
+
+## Task
+
+Prepare your development environment for native Node.js add-ons
+
+──────────────────────────────────────────────────────────────────────
+
+## Description
+
+To pass this exercise you simply need to have a development environment with all
+the required tools installed.
+
+You need:
+
+A recent and stable version of Node.js: unstable releases are not tested with
+this workshop and should be avoided. Visit [http://nodejs.org/](http://nodejs.org/)
+to download a new version or use your local package manager to upgrade.
+
+A compiler: specifically, a (non-ancient) C++ compiler, we will require gcc or
+LLVM gcc on OS X. Use your native package manager to install build tools
+including gcc.
+
+Python: version 2.7.x is preferred for use by GYP, a build generation tool used
+for configuring and compiling Node.js add-ons. Download from
+<https://www.python.org/download/releases/2.7.7> or use your package manager.
+
+node-gyp: a recent version installed globally with sudo npm install node-gyp -g.
+node-gyp wraps GYP and produces builds specifically targeted for Node.js.
+Install with sudo npm install -g node-gyp.
+
+If you are unsure, simply run goingnative verify and we'll tell you what versions
+you have.
+
+──────────────────────────────────────────────────────────────────────
+
+ » To print these instructions again, run: goingnative print
+ » To verify you have the correct environment, run: goingnative verify
+ » For help run: goingnative help
+
+*/
