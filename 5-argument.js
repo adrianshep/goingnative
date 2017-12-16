edcd5728799d8bdee88bec29095a76ad7b66e77b
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-12-16 09:43:19 -0500

    5th lesson in goingnative

--- 5-argument.js
+++ 5-argument.js
@@ -3,6 +3,20 @@
 
 /*
 
+$ goingnative verify myaddon
+✓ package.json contains `"gypfile": true`
+✓ binding.gyp includes a "myaddon" target
+✓ binding.gyp includes "myaddon.cc" as a source file
+✓ binding.gyp includes a correct NAN include statement
+xcode-select: error: tool 'xcodebuild' requires Xcode, but active developer directory '/Library/Developer/CommandLineTools' is a command line tools instance
+
+xcode-select: error: tool 'xcodebuild' requires Xcode, but active developer directory '/Library/Developer/CommandLineTools' is a command line tools instance
+
+make: *** No rule to make target `Release/obj.target/myaddon/myaddon.o', needed by `Release/myaddon.node'.  Stop.
+✗ node-gyp build: `make` failed with exit code: 2
+
+
+
 FOR THE SAKE OF ARGUMENT
  Exercise 5 of 8
 
