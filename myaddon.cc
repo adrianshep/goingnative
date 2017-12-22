8f3da4831c33e28ac440550efd4b1fb60ac850ed
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-12-22 10:28:13 -0500

    7th lesson in goingnative

--- myaddon.cc
+++ myaddon.cc
@@ -5,20 +5,32 @@
 
 using namespace v8;
 
-NAN_METHOD(Print) {
-  Nan::MaybeLocal<String> maybeStr = Nan::To<String>(info[0]);
-  v8::Local<String> str;
-  if (maybeStr.ToLocal(&str) == false) {
-    Nan::ThrowError("Error converting first argument to string");
-  }
-  int len = strlen(*String::Utf8Value(str));
+NAN_METHOD(Delay) {
+  Nan::Maybe<int> maybeDelay = Nan::To<int>(info[0]);
 
-  info.GetReturnValue().Set(len);
+  if (maybeDelay.IsNothing() == true) {
+    Nan::ThrowError("Error converting first argument to integer");
+  }
+
+  int delay = maybeDelay.FromJust();
+
+  if (info[1]->IsFunction() == false) {
+    Nan::ThrowError("Error converting second argument to function");
+  }
+
+  #ifdef _WIN32
+    Sleep(delay);
+  #else
+   usleep(delay * 1000);
+  #endif
+
+  v8::Local<Function> callback = info[1].As<Function>();
+  Nan::MakeCallback(Nan::GetCurrentContext()->Global(), callback, 0, NULL);
 }
 
 NAN_MODULE_INIT(Init) {
-  Nan::Set(target, Nan::New("length").ToLocalChecked(),
-      Nan::GetFunction(Nan::New<FunctionTemplate>(Print)).ToLocalChecked());
+  Nan::Set(target, Nan::New("delay").ToLocalChecked(),
+      Nan::GetFunction(Nan::New<FunctionTemplate>(Delay)).ToLocalChecked());
 }
 
 NODE_MODULE(myaddon, Init)
