f442c5b38fc09b879ec66f42e647afd5d5b9be55
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-12-27 09:36:29 -0500

    8th lesson in goingnative

--- myaddon.cc
+++ myaddon.cc
@@ -1,9 +1,40 @@
 #include <nan.h>
 #ifndef _WIN32
-#include <unistd.h>
+# include <unistd.h>
 #endif
 
 using namespace v8;
+
+class MyWorker : public Nan::AsyncWorker {
+ public:
+  MyWorker(Nan::Callback *callback, int delay)
+    : Nan::AsyncWorker(callback), delay(delay) {}
+  ~MyWorker() {}
+
+  // Executed inside the worker-thread.
+  // It is not safe to access V8, or V8 data structures
+  // here, so everything we need for input and output
+  // should go on `this`.
+  void Execute () {
+    #ifdef _WIN32
+     Sleep(delay);
+    #else
+     usleep(delay * 1000);
+    #endif
+  }
+
+  // Executed when the async work is complete
+  // this function will be run inside the main event loop
+  // so it is safe to use V8 again
+  void HandleOKCallback () {
+    Nan::HandleScope scope;
+
+    callback->Call(0, NULL);
+  }
+
+ private:
+  int delay;
+};
 
 NAN_METHOD(Delay) {
   Nan::Maybe<int> maybeDelay = Nan::To<int>(info[0]);
@@ -12,20 +43,17 @@ NAN_METHOD(Delay) {
     Nan::ThrowError("Error converting first argument to integer");
   }
 
-  int delay = maybeDelay.FromJust();
-
   if (info[1]->IsFunction() == false) {
     Nan::ThrowError("Error converting second argument to function");
   }
 
-  #ifdef _WIN32
-    Sleep(delay);
-  #else
-   usleep(delay * 1000);
-  #endif
+  int delay = maybeDelay.FromJust();
 
   v8::Local<Function> callback = info[1].As<Function>();
-  Nan::MakeCallback(Nan::GetCurrentContext()->Global(), callback, 0, NULL);
+
+  Nan::Callback* nanCallback = new Nan::Callback(callback);
+  MyWorker* worker = new MyWorker(nanCallback, delay);
+  Nan::AsyncQueueWorker(worker);
 }
 
 NAN_MODULE_INIT(Init) {
